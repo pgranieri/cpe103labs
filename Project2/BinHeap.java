37645592d3990fea2d1df3ce2636375fac957da4
Patrick's Linux <pgranier@calpoly.edu>
2015-10-21 17:47:10 -0700

    Completed rough draft of BinHeap

--- Project2/BinHeap.java
+++ Project2/BinHeap.java
@@ -15,18 +15,18 @@ public class BinHeap <T extends Comparable<? super T>> {
 
 
 	public BinHeap(){
-		T[] heap = (T[]) new Comparable[100];
+		heap = (T[]) new Comparable[100];
 		heapsize = 0;
 	}
 
 	public BinHeap(int size){
-		T[] heap = (T[]) new  Comparable[size];
+		heap = (T[]) new  Comparable[size];
 		heapsize = 0;
 	}
 
 	public void insert(T element){
 		if(heapsize == heap.length){
-			T[] temp = (T[]) new  Object[heap.length*2];
+			T[] temp = (T[]) new  Comparable[heap.length*2];
 			for(int i = 0; i < heap.length; i++){
 				temp[i] = heap[i];
 			}
@@ -37,16 +37,18 @@ public class BinHeap <T extends Comparable<? super T>> {
 		T hole = heap[holeIndex];
 		int parentIndex = (heapsize-1)/2;
 		T parent = heap[parentIndex];
-		//We need to use the compareTo method, we have to deal with comparable objects.
-		while (parentIndex >= 0 && !isParentValid(element,parent)) {
-			hole = parent;
+
+		while (holeIndex > 0 && !isParentValid(element,parent)) {
+			heap[holeIndex] = parent;
 			holeIndex = parentIndex;
 			parentIndex = (parentIndex-1)/2;
 			parent = heap[parentIndex];
 		}
-		hole = heap[holeIndex];
+
+		heap[holeIndex] = element;
 		heapsize++;
 	}
+
 
 	private boolean isParentValid(T child, T parent){
 		boolean result;
@@ -64,40 +66,32 @@ public class BinHeap <T extends Comparable<? super T>> {
 		if(heapsize == 0){
 			throw new MyException(); 
 		}
-		T returnValue = heap[0]; //the root of the tree -> smallest item in heap
-		T item; // the item that will be swapped through the holes
-		int itemIndex; // the item's index
-		int indexLP = heapsize/2 - 1; // LP = largest parent "deepest rightmost parent"
-		int leftChild = 2*indexLP+1;
-		int rightChild = 2*indexLP+2;
-		if(heap[leftChild].compareTo(heap[rightChild]) > 0){ // checking larger child to replace root
-			item = heap[leftChild];
-			itemIndex = leftChild;
-		} else{ // if they are equal we should take the right child??***
-			item = heap[rightChild];
-			itemIndex = rightChild;
-		}
+		T returnValue = heap[0]; //the root of the tree -> smallest item in heap		
+		int itemIndex = heapsize-1; // the item's index
+		T item = heap[itemIndex]; // the item that will be swapped through the holes
 		heapsize--;//decrease size of collection
 		int hole = 0; // set hole at root
 
 		// after this point i am having a little trouble/confusion
-		int nHole =newHole(hole,itemIndex);		
+		int nHole =newHole(hole,item);		
 		while(nHole != -1){
 			heap[hole] = heap[nHole]; // specifially with this line of code
 			hole = nHole;
-			nHole =newHole(hole,itemIndex);
+			nHole =newHole(hole,item);
+			//System.out.println(newHole(hole,item) + "   new hole ------");
 		}
+		//System.out.println(hole + "   holes index");
 
 		heap[hole] = item;
 		return returnValue;
 	}
 
-	private int newHole(int hole, int item){ // both paramaters are indexes
+	private int newHole(int hole, T item){ // both paramaters are indexes
 		int returnIndex = -1;
 		int smallestChild;
 		if(2*hole +1 <= heapsize){ // left child of hole within collection
 			if(2*hole +2 > heapsize){// right child of hole NOT within collection
-				if(heap[2*hole+1].compareTo(heap[item]) < 0){
+				if(heap[2*hole+1].compareTo(item) < 0){
 					returnIndex = 2*hole+1; //left child is new hole
 				}				
 			}else{ //the hole has 2 children
@@ -106,7 +100,7 @@ public class BinHeap <T extends Comparable<? super T>> {
 				}else{
 					smallestChild = 2*hole+2; //right
 				}
-				if(heap[smallestChild].compareTo(heap[item]) < 0){ // compare smallest child to item
+				if(heap[smallestChild].compareTo(item) < 0){ // compare smallest child to item
 					returnIndex = smallestChild;
 				}
 			}
