e80292c912f55b67c2fb60ced353828422c3ae35
rottren <ngatehou@calpoly.edu>
2015-10-30 11:35:30 -0700

    skeleton of project 3, BST.java, LQueue.java, & AStack.java

---
+++ Project3/BST.java
@@ -0,0 +1,97 @@
+//	Authors: Patrick Granieri and Nick Gatehouse
+//	ID's: pgranier@calpoly.edu and ngatehou@calpoly.edu
+//	Project 3,  10/29/2015
+
+public class BST <T extends Comparable<? super T>> {
+	private class BSTNode{
+		T element;
+		BSTNode leftChild;
+		BSTNode rightChild;
+
+		public BSTNode(int element, BSTNode leftChild,BSTNode rightChild){
+			this.element = element;
+			this.leftChild = leftChild;
+			this.rightChild = rightChild;			
+		}
+	}
+	private BSTNode root;
+
+	public BST(){
+		root = null;
+	}
+
+	private class PreIter{
+
+	}
+
+	private class InIter{
+
+	}
+
+	private class LevelIter{
+
+	}
+
+	public void insert(T item){
+
+	}
+
+	public void delete(T item){
+
+	}
+
+	public boolean find(T item){
+
+	}
+
+	public boolean isEmpty(){
+
+	}
+
+	public void makeEmpty(){
+
+	}
+
+	public T findMinimum(){
+
+	}
+
+	public T findMaximum(){
+
+	}
+
+	public Iterator<T> iteratorPre(){
+
+	}
+
+
+	public Iterator<T> iteratorIn()){
+		
+	}
+
+
+	public Iterator<T> iteratorLevel(){
+		
+	}
+
+	public void printTree(){
+
+	}
+
+	public String toString(){
+
+	}
+
+	public static class MyException extends RuntimeException{
+		// Custom Error primarily used for empty cases. 
+
+        public MyException(){
+        	//Default constructor used if no output text is given.
+        	super();
+        }
+        public MyException(String message){
+        	//Takes in message as an error message for custom output.
+        	super(message);
+        }
+	}	
+}
---
+++ Project3/LQueue.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class LQueue<T> {
+
+	public LQueue(){
+		front = null;
+		end = null;
+	}
+	private class Node<T>{
+		public T element;
+		public Node<T> next;
+		public Node(T value,Node node){
+			element = value;
+			next = node;
+		}
+	}	
+    private Node<T> front;
+    private Node<T> end;
+	public void enqueue(T item){
+		if (isEmpty()){
+			Node<T> blah = new Node(item, null);
+			front = blah;
+			end = blah;
+		} else {
+			Node<T> temp = new Node(item,null);
+			end.next=temp;
+			end = temp;
+		}
+		
+	}
+	public T dequeue(){
+		if(isEmpty()){
+			throw new MyException("queue empty"); 
+		}
+		else{
+			T temp = front.element;
+			front = front.next;
+			return temp;
+		}
+	}
+	
+	public boolean isEmpty(){
+		if(front == null){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	public static class MyException extends RuntimeException{
+        public MyException(){
+        	super();
+        }
+        public MyException(String message){
+        	super(message);
+        }
+	}
+}
