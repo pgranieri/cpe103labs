b88d7b0c9e6a19fbc7838fa10ae7cbd0292ef9dc
rottren <ngatehou@calpoly.edu>
2015-11-15 22:30:40 -0800

    lab 10 finished, time to test it

--- NicksWorkMT2/L10_BSTWork.java
+++ NicksWorkMT2/L10_BSTWork.java
@@ -1,3 +1,68 @@
+import java.io.*;
+
 public class L10_BSTWork{
-	
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		L10_BasicBST basic = new L10_BasicBST();
+
+		System.out.println("Choose one of the following operations: )");
+		System.out.println("-	add (Enter the letter 'a')");
+		System.out.println("-	odds (Enter the letter 'o')");
+		System.out.println("-	height (enter the letter 'h')");
+		System.out.println("-	leaves (enter the letter 'l')");
+		System.out.println("-	one child nodes  (enter the letter 'c')");
+		System.out.println("-	quit (enter the letter 'q')");
+
+		String answer;
+		boolean cont = true;
+
+		while(cont) {
+			System.out.println("Enter a Menu Choice: (a,o,h,l,c,q)");
+			answer = input.nextLine();
+			switch (answer){
+				case "a": 
+					System.out.println("Input a value to be added: ");
+
+					if (input.hasNextInt()) {
+						int ad = input.nextInt();
+						basic.insert(ad);
+
+						System.out.println(ad + " Add");						
+					} else {
+						System.out.println("Invalid Value.");
+					}
+					input.nextLine();
+					break;
+
+				case "o":
+					int odds = basic.countOdds(); 
+					System.out.println( odds + " is the number of odds");					
+					break;
+
+					
+				case "h":
+					System.out.println("The height of the tree is " + basic.height());
+					break;
+
+
+				case "l":
+					System.out.println("The number of leaves is " + basic.countLeaves());					
+					break;
+
+				case "c":
+					System.out.println("The number of one child nodes is " + basic.countOneChildParents());
+					break;
+
+				case "q":
+					cont = false;
+					System.out.println("Quitting.");
+					break;
+
+				default:
+					System.out.println("Invalid Choice.");
+					break;
+			}
+		}
+	}
 }
--- NicksWorkMT2/L10_BasicBST.java
+++ NicksWorkMT2/L10_BasicBST.java
@@ -1,3 +1,118 @@
+import java.Math.*;
+
 public class L10_BasicBST{
-	
+	private class BSTNode{
+		int element;
+		BSTNode left;
+		BSTNode right;
+
+		public BSTNode(int item){
+			this.element = item;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+	private BSTNode root;
+
+	public L10_BasicBST(){
+		root = null;
+	}
+
+	// use recursion
+
+	public void insert(int item){
+		root = insert(item,root);
+	}
+
+	private BSTNode insert(int item, BSTNode treeroot){
+		if(treeroot == null){
+			treeroot = new BSTNode(item);
+		}else{
+			if(item < treeroot.element){
+				treeroot.left = insert(item,treeroot.left);
+			}else{
+				treeroot.right = insert(item,treeroot.right);
+			}
+		}
+		return treeroot;
+	}
+
+	public int countOdds(){
+		return countOdds(root);
+	}
+
+	private int countOdds(BSTNode treeroot){
+		int answer;
+
+		if (treeroot == null){
+			answer = 0;
+		}else{
+
+			if(treeroot.left != null && (treeroot.left.element % 2 != 0)){
+				answer = countOdds(treeroot.left) + 1;
+			}
+
+			if(treeroot.right != null && (treeroot.right.element % 2 != 0)){
+				answer = countOdds(treeroot.right) + 1;
+			}
+		}
+		return answer;
+	}
+
+	public int height(){
+		if(root == null){
+			return -1;
+		}else{
+			return height(root) -1 ;	
+		}
+		
+	}
+
+	private int height(BSTNode treeroot){
+		int result;
+		if(treeroot == null){
+			reslut = 0;
+		}else{
+			result = 1 + Math.max(height(treeroot.left),height(treeroot.right));
+		}
+		return result;
+	}
+
+	public int countLeaves(){
+		return countLeaves(root);
+	}
+
+	private BSTNode countLeaves(BSTNode treeroot){
+		int leaves;
+
+		if(treeroot == null){
+			leaves = 0;
+		}else{
+			if(treeroot.right == null && treeroot.left == null){ // if both its childeren are null then it is a leaf
+				leaves = 1; // how does this add up? you are just assigning it, shouldnt it be +=?
+			}else{
+				leaves = countLeaves(treeroot.left) + countLeaves(treeroot.right); // each time it's called both values can only be one...this is where the recursion adds them i believe
+			}
+		}
+
+	}
+
+	public int countOneChildParents(){
+		return countOneChildParents(root);
+	}
+
+	private int countOneChildParents(BSTNode treeroot){
+		int result;
+		if(treeroot == null){
+			return 0;
+		}else {
+			if((treeroot.left != null && treeroot.right == null) || (treeroot.left == null && treeroot.right != null)){
+				result = 1;
+			}else{
+				result = countOneChildParents(treeroot.left) + countOneChildParents(treeroot.right);
+			}
+		}
+		return result;
+	}
 }
